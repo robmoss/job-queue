ccacb33f8d65ec4f906e78316e914a68ba4777fd
Rob Moss <robm.dev@gmail.com>
2022-12-14 11:37:35 +1100

    Improve the Ctrl+C test case
    It now checks that sending a Ctrl+C signal causes the workers to fail
    and that without sending a Ctrl+C signal the workers instead succeed.

--- tests/test_ctrl_c.py
+++ tests/test_ctrl_c.py
@@ -2,6 +2,7 @@ import logging
 import multiprocessing
 import os
 import signal
+import sys
 
 import parq
 
@@ -15,17 +16,28 @@ def test_ctrl_c_handled():
         if n == 5:
             os.kill(pid, signal.SIGINT)
 
-    def run_queue():
+    def run_queue(count):
         logger = multiprocessing.log_to_stderr()
         logger.setLevel(logging.WARNING)
 
         pid = os.getpid()
-        values = [(i, pid) for i in range(20)]
+        values = [(i, pid) for i in range(count)]
         n_proc = 3
 
         success = parq.run(func, values, n_proc=n_proc)
-        assert not success
+        if success:
+            sys.exit(0)
+        else:
+            sys.exit(42)
 
-    proc = multiprocessing.Process(target=run_queue, args=[])
+    # Ensure that Ctrl+C causes the workers to fail.
+    proc = multiprocessing.Process(target=run_queue, args=[20])
     proc.start()
     proc.join()
+    assert proc.exitcode == 42
+
+    # Ensure that without Ctrl+C the workers succeed.
+    proc = multiprocessing.Process(target=run_queue, args=[4])
+    proc.start()
+    proc.join()
+    assert proc.exitcode == 0
